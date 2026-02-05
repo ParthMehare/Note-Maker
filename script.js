b8fb4a5acc38ee5f8373d7380ffc8a32412821be
[Parth] <[parthmehare11@gmail.com]>
2026-02-05 21:53:03 +0530

    Fixed hide issues

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 let btn1 = document.querySelector("#open");
 let form_container = document.querySelector(".form-container");
 let wrapper = document.querySelector(".wrapper");
+let create_btn = document.querySelector(".create");
+let form = document.querySelector("form");
+let close_btn = document.querySelector(".close");
 
-btn1.addEventListener("click", ()=>{
-    form_container.style.display = "initial";
-    wrapper.style.display = "none";
-})
+btn1.addEventListener("click", () => {
+  form_container.classList.remove("hide");
+  wrapper.classList.add("hide");
+});
+
+form.addEventListener("submit", (inp) => {
+  inp.preventDefault();
+  form.reset();
+});
+
+close_btn.addEventListener("click", () => {
+  form_container.classList.add("hide");
+  wrapper.classList.remove("hide");
+});
